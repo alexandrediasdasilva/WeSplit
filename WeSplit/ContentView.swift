e14cc8cef1bd5952eaeb690f09c23108b99a3f44
alexandre.diasdasilva <alexandre.diasdasilva@yahoo.fr>
2021-05-20 22:53:16 +0200

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Alexandre Dias Da Silva on 20/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
